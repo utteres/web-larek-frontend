06e4f67c81d7ca068233c37bcec064711d439fff
vyatscheslav vyazov <vyatscheslav.vyazov@gmail.com>
2024-02-26 18:57:56 +0300

    архитектура

---
+++ src/types/index.ts
@@ -0,0 +1,39 @@
+export interface IProduct{
+    id: string,
+    description: string,
+    image: string,
+    title: string,
+    category: string,
+    price: number;
+    error?:string
+}
+
+type PaymentMethod = 'online' | 'offline';
+
+export interface IOrderForm { 
+    email: string; 
+    phone: string; 
+    address: string; 
+    payment: PaymentMethod; 
+} 
+
+export interface IOrder extends IOrderForm {
+    items: string[];
+    total:number;
+}
+
+export interface IOrderResult {
+    id: string;
+    total:number;
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface IAppState {
+    catalog: IProduct;
+    basket: string[];
+    preview: string | null;
+    order: IOrder | null;
+}
+
+export type IBasketItem = Pick<IProduct, 'id' | 'title' | 'price'>
